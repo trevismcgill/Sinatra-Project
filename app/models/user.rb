28dcaf4f28dabcd78fc78778623f64e8b51fd5cf
trevismcgill <trevismcgill25@gmail.com>
2020-09-18 15:37:36 -0500

    Added Sinatra-Flash Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
+    validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true
     has_many :board_games
 
